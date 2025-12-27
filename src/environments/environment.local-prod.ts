8f977fa077aa44ed62e2eebb0207ba994abb182d
vhuthuphuthu <66826074+vhuthuphuthu@users.noreply.github.com>
2025-12-27 23:32:38 +0200

    fix dev url

--- src/environments/environment.local-prod.ts
+++ src/environments/environment.local-prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ylkgde9us8.execute-api.eu-west-1.amazonaws.com/prod',
+  apiUrl: 'https://ylkgde9us8.execute-api.eu-west-1.amazonaws.com/dev',
   version: '1.0.0',
   buildDate: new Date().toISOString(),
   features: {
