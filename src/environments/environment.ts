fcd0edee449d395fb89f4b033eb263e359efe360
vhuthuphuthu <66826074+vhuthuphuthu@users.noreply.github.com>
2026-06-23 21:20:45 +0200

    update lambda

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ylkgde9us8.execute-api.eu-west-1.amazonaws.com/dev',
+  apiUrl: 'https://b9otcnq1xe.execute-api.eu-west-1.amazonaws.com/dev',
   googleClientId: '764511640841-c9tochdbm0ssq9em0afp0fb9pnm779rp.apps.googleusercontent.com',
   version: '1.0.0',
   buildDate: new Date().toISOString(),
